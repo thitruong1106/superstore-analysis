a2861755f61d8d42f057e3126b85528f06079e5a
thitruong1106 <51068289+thitruong1106@users.noreply.github.com>
2026-05-01 00:27:03 +0900

    Create 01_category_performance.sql

---
+++ data/sql/01_category_performance.sql
@@ -0,0 +1,8 @@
+SELECT 
+	Category, 
+	ROUND(SUM(Sales),2) as total_sales,
+	ROUND(SUM(Profit),2) as total_profit, 
+	ROUND(SUM(Profit) / NULLIF(SUM(Sales),0),2) * 100 as profit_margin_pct, 
+	RANK() OVER (ORDER BY SUM(Sales)DESC) as sales_rank
+FROM orders 
+GROUP BY  Category;
