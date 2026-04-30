089dc8cca5a02eba13589faf8e80afff92f84cbd
thitruong1106 <51068289+thitruong1106@users.noreply.github.com>
2026-05-01 00:32:29 +0900

    Update 02_monthly_trends.sql

--- data/sql/02_monthly_trends.sql
+++ data/sql/02_monthly_trends.sql
@@ -1,7 +1,8 @@
 SELECT 
 	YEAR(order_date_clean) as year,
 	MONTH(order_date_clean) as month, 
-	SUM(Sales) as total_sale
+	ROUND(SUM(Sales),2) as total_sale, 
+	ROUND(SUM(Profit),2) as total_profit
 FROM orders
 GROUP BY YEAR(order_date_clean), MONTH(order_date_clean)
 ORDER BY YEAR(order_date_clean), MONTH(order_date_clean); 
